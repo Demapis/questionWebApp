bc3c913b6aba6ddff4e85303ce2b6077ef1bf990
Victor Owino <vo4685336@gmail.com>
2025-02-04 01:24:03 +0300

    update ALLOWED_HOSTS to allow connections from all hosts

--- questionWebApp/settings.py
+++ questionWebApp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-h&lmjs*nhyy)*@m5b%&@956vpqojsc8pm9i_l%_!smh3yd6tq=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
